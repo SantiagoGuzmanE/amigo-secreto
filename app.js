067a3b4c25d2b953ef75d70305f7d6f5ec779f2d
Santiago Guzman <santiagoguz42@gmail.com>
2025-08-06 14:31:52 -0500

    Funcion para crear amigos

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-prueba
+function agregarAmigos(){
+    let amigo = document.getElementById('amigo').value;
+        if (amigo == ""){
+            alert('Por favor ingrese un nombre')
+        }else{
+            amigos.push(amigo);
+            document.getElementById('amigo').value = "";
+            console.log(amigos);
+        }
+}
+function actualizarAmigos(){
+    let 
+}
